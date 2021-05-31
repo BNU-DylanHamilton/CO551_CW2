99253323ba77ad598eca64430a9f7d4574a7a8ca
BNU-DylanHamilton <60743070+BNU-DylanHamilton@users.noreply.github.com>
2021-05-31 18:58:25 +0100

    Updating nav.php

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -2,7 +2,9 @@
 <div id="main-nav">
 
    <a href="modules.php">My Modules</a> |
-   <a href="assignmodule.php">Assign Module</a> |
+   <a href="assignmodule.php">Assign Module</a>|
+    <a href="students.php">Students</a> |
+    <a href="addstudent.php">Add Student</a> |
    <a href="details.php">My Details</a> |
    <a href="logout.php">Logout</a>
 
